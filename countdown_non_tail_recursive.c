1bab75be9afcbb7a4238b08f1295dad7e2185404
HilarioNengareJr <38634516+HilarioNengareJr@users.noreply.github.com>
2022-10-23 18:05:51 +0200

    Update countdown_non_tail_recursive.c

--- countdown_non_tail_recursive.c
+++ countdown_non_tail_recursive.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
 
-//Write a C program that will ask to enter an integer number from the user and will countdown until 0. At the end, it will print «Time is up!».
+//Write a C program thatwillask toenteran integer numberfromtheuserandwillcountdownuntil0. At the end, it will print«Time is up!».
 
-//Recursive
-int countdown(int n){
+//Non-Tail Recursive
+void countdown(unsigned int n){
 
 	// Base condition
 	if (n<=0){
 		printf("\nTime Is Up!\n");
 		return 0;
 	}
-	printf("\n%d\n", n);
+	
+	printf("\n%d\n", n); //pending operation
 	//Recursive condition
-	return countdown(n-1);
+	countdown(n-1);
+
 		
 } 
 
+
+//Tail Recursive with an accummulat
+int countdown_tail_recursive(unsigned int n, unsigned int acc_val)
+{
+	acc_val = n;
+	
+	if(acc_val==0)
+	{
+		printf("\nTime Is Up!\n");
+		return 0;
+	}	
+	printf("\n%d\n", n);
+	//Recursive condition
+	return countdown_tail_recursive(n-1, acc_val); 
+	//no pending operation
+	
+}
+
+int ctr(unsigned int n)
+{
+	return countdown_tail_recursive(n,n);	// makes countdown_tail_recursive() the last operation and therefore no pending operation
+}
+
 //Iterative
-int countdown_b(int n){
+int countdown_b(unsigned int n){
 	
 	int i;
 	
@@ -29,14 +54,20 @@ int countdown_b(int n){
 	return 0;
 }
 
+
 int main()
 {
 	int number;
+	int x = number;
+	
 	printf("Enter a Number\t");
 	scanf("%d", &number);
 	
-	printf("\nRecursive Result\n");
-	printf(countdown(number));
+	printf("\nNon-Tail Recursive\n");
+	countdown(number);
+	
+	printf("\nTail Recursive Result\n");
+	printf(ctr(number));
  	
  	printf("\nIterative Result:\n");
  	printf(countdown_b(number));
